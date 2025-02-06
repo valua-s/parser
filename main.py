d31f68435a4f82c9b8c55f611fa26af7e141745d
Valua-s <speranskaya.valya@yandex.ru>
2025-02-06 15:42:11 +0500

    after refactoring by review

--- main.py
+++ main.py
@@ -1,62 +1,69 @@
+import logging
 import requests
 from time import sleep
 from bs4 import BeautifulSoup
 import csv
 
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 
 BASE_URL = "magbo.ru"
 
 
-def search_products(query, region_name=''):
-    q_set = query.split(' ')
-    q_request = q_set[0]
-    for q in q_set[1:]:
-        q_request += f'+{q}'
+def search_products(query: str, region_name='') -> list:
+    q_request: str = query.replace(' ', '+')
     if region_name:
         region_name += '.'
     full_url = f"https://{region_name}{BASE_URL}"
     search_url = f"{full_url}/catalog/?q={q_request}&s=Найти"
-
-    response = requests.get(search_url)
-    sleep(1)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
-    # pages = int(soup.select('[href*="PAGEN_2"]')[-1].text)
-    
+    try:
+        response = requests.get(search_url)
+    except Exception as e:
+        logger.error(e)
+        sleep(3)
+    soup: BeautifulSoup = BeautifulSoup(response.text, 'html.parser')
+    try:
+        pages = int(soup.select('[href*="PAGEN_2"]')[-1].text)
+    except IndexError:
+        pages = None
     product_links = []
-    
     for link in soup.select('.dark_link.option-font-bold.font_sm'):
         product_links.append(link.get('href'))
-        
-    # if pages:
-    #     for page in range(2, pages+1):
-    #         search_url += f'&PAGEN_2={page}'
-    #         response = requests.get(search_url)
-    #         soup = BeautifulSoup(response.text, 'html.parser')
-    #         for link in soup.select('.dark_link.option-font-bold.font_sm'):
-    #             product_links.append(link.get('href'))
-    
+
+    if pages:
+        for page in range(2, pages+1):
+            search_url += f'&PAGEN_2={page}'
+            response = requests.get(search_url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            for link in soup.select('.dark_link.option-font-bold.font_sm'):
+                product_links.append(link.get('href'))
+
     return product_links
 
-def parse_product(product_url: str, region_name):
+
+def parse_product(product_url: str, region_name: str = '') -> dict:
     if region_name:
         region_name += '.'
-    full_product_url = f"https://{region_name}{BASE_URL}/{product_url}"
-    response = requests.get(full_product_url)
-    sleep(1)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
-    title = soup.select_one('h1').get_text(strip=True).replace('"', '')
-    price_with_discount = soup.select_one('.price_value_block.values_wrapper > .price_value').get_text(strip=True)
+    full_product_url = f"https://{region_name}{BASE_URL}{product_url}"
     try:
-        price_without_discount = soup.select_one('.discount.values_wrapper.font_xs.muted > .price_value').get_text(strip=True)
+        response = requests.get(full_product_url)
+    except Exception as e:
+        logger.error(e)
+        sleep(3)
+    soup: BeautifulSoup = BeautifulSoup(response.text, 'html.parser')
+
+    title: str = soup.select_one('h1').get_text(strip=True).replace('"', '')
+    price_with_discount: str = soup.select_one('.price_value_block.values_wrapper > .price_value').get_text(strip=True)
+    try:
+        price_without_discount: str = soup.select_one('.discount.values_wrapper.font_xs.muted > .price_value').get_text(strip=True)
     except Exception:
         price_without_discount = price_with_discount
         price_with_discount = None
-    article = soup.select_one('.properties__value.darken.properties__item--inline.js-prop-value').get_text(strip=True)
+    article: str = soup.select_one('.properties__value.darken.properties__item--inline.js-prop-value').get_text(strip=True)
     
-    manufacturer = soup.find_all('span', class_='js-prop-value')[3].get_text(strip=True)
-    availability = soup.select_one('.value.font_sxs').get_text(strip=True)
+    manufacturer: str = soup.find_all('span', class_='js-prop-value')[3].get_text(strip=True)
+    availability: str = soup.select_one('.value.font_sxs').get_text(strip=True)
     
     return {
         'title': title,
@@ -68,30 +75,31 @@ def parse_product(product_url: str, region_name):
         'url': full_product_url,
     }
 
-def save_to_csv(data: list, filename='products.csv'):
+
+def save_to_csv(data: list, filename: str = 'products.csv') -> None:
     keys = data[0].keys()
     with open(filename, 'w', newline='', encoding='utf-8') as output_file:
         dict_writer = csv.DictWriter(output_file, fieldnames=keys)
         dict_writer.writeheader()
         dict_writer.writerows(data)
 
-def main(queries: dict, region_names: dict):
-    all_products = []
+
+def main(queries: dict, region_names: dict) -> None:
+    
+    all_products: list = []
     
     for query in queries:
         for region_name in region_names:
-            product_links = search_products(query, region_name)
+            product_links: list = search_products(query, region_name)
         
             for product_link in product_links:
-                product_data = parse_product(product_link, region_name)
+                product_data: dict = parse_product(product_link, region_name)
                 all_products.append(product_data)
     
     save_to_csv(all_products)
 
 
 if __name__ == "__main__":
-    queries = ["Смеситель Kerama Marazzi", "Биде Sole"]
-    region_names = ['']
-    # region_names = ['bahchisarai', 'evpatoria', 'kerch', 'saki', 'sevastopol', '', 'feodosia', 'yalta']
-    main(queries, region_names)
-    
+    queries: list = ["Биде Sole", "Смеситель Kerama Marazzi"]
+    # all_region_names = ['bahchisarai', 'evpatoria', 'kerch', 'saki', 'sevastopol', '', 'feodosia', 'yalta']
+    main(queries,)
